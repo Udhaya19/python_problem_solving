4aaebdb3af7322b245b5fd3cdf4cb104fef66f6b
Udhaya19 <banusankar17@gmail.com>
2019-04-23 11:10:20 +0530

    Added the comment line for assignment problem

--- python_problem_solving/assignment_problem/assignment1.py
+++ python_problem_solving/assignment_problem/assignment1.py
@@ -1,10 +1,11 @@
 import datetime
-
 import pandas
 
 store = pandas.read_csv('/home/udhaya/Downloads/store.csv')
 location = pandas.read_csv('/home/udhaya/Downloads/location.csv')
 
+
+# a. List all stores which have ​ build_area ​ greater than 4000
 
 def stores_greater_than_4000():
     for key, row in store.iterrows():
@@ -15,11 +16,13 @@ def stores_greater_than_4000():
 stores_greater_than_4000()
 
 
+# b. Find ​ country​ having the most number of stores
 def number_stores():
     i = 0
-    store_count = {}
     store_ = []
-
+    # store_=[pin_number]
+    store_count = {}
+    # store_count={"pin_number":count}
     for key, value in store.iterrows():
         store_.append(value[3])
     for pin_number in store_:
@@ -29,12 +32,16 @@ def number_stores():
 
 def most_number_stores(i, store_count):
     store_value = {}
+    # store_value={"pin code":"city name"}
     maximum_number_list = []
+    # maximum_number_list=[count of store]
     high_value = []
+    # high_value=[pin code of maximum number]
     country_list = []
     for row, column in store_count.items():
         maximum_number_list.append(column)
     maximum_number_list = sorted(maximum_number_list, reverse=True)
+    # maximum_number_list=[reversed number count of store]
     for row, column in store_count.items():
         if column == maximum_number_list[i]:
             high_value.append(row)
@@ -52,6 +59,8 @@ def most_number_stores(i, store_count):
 
 number_stores()
 
+
+# c. Find the total ​ build_area​ of all stores built last year
 
 def total_build_area_last_year():
     now = datetime.datetime.now()
@@ -77,11 +86,15 @@ def total_build_area_last_year():
 total_build_area_last_year()
 
 
+# d. Find all stores which were opened on a ​ weekend
+
 def store_opened_weekend():
     weekend_day1 = 'Saturday'
     weekend_day2 = 'Sunday'
     build_area_dict = {}
+    # build_area_dict={"storename":"weekdays"}
     store_weekend_opened = []
+    # store_weekend_opened=[store_name]
     for key, value in store.iterrows():
         date_value = value[2]
         month, day, year = (int(x) for x in date_value.split('/'))
@@ -104,13 +117,19 @@ def store_opened_weekend():
 store_opened_weekend()
 
 
+# e. List stores which are located in a city containing character `​ z ​ ` in it
+
 def city_contain_z_character():
     city_contain_z = []
+    # city_contain_z=[city names]
     s1 = 'z'
     s2 = 'Z'
     character = []
+    # charater=[only have a z character]
     pin_code = []
+    # pin_code=[z character pincode]
     result_store_list = []
+    # result_store_list=[store_name city have z character]
     for key, value in location.iterrows():
         city_contain_z.append(value[1])
     for i in range(0, len(city_contain_z)):
@@ -130,11 +149,17 @@ def city_contain_z_character():
 city_contain_z_character()
 
 
+# f. Calculate ​ number of stores​ in each ​ City
+
 def store_number_city():
     pin_number = []
+    # pin_number=[list of pincode]
     pin_code_occurence = {}
+    # pin_code_occurence={"pincode":"count of pincode"}
     city_name = {}
+    # city_name={"pincode":"city name"}
     final_store_count = {}
+    # final_store_count={"city name":"store count"}
     for key, value in store.iterrows():
         pin_number.append(value[3])
     for pin_code in pin_number:
